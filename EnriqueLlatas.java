b68031862dde511e8755c85456da8216770dc524
Enrique <u20201f172@upc.edu.pe>
2023-08-19 10:43:12 -0500

    Clase  EnriqueLlatas Adding constructor w

---
+++ EnriqueLlatas.java
@@ -0,0 +1,16 @@
+public class EnriqueLlatas{
+
+    private Integer id;
+
+    private String lastName;
+    
+    private String firstName;
+
+    public EnriqueLlatas(){
+
+    }
+    
+    public Integer getId(){
+        return id;
+    }
+}
