3cf7ed77ba1a22874f9b2770a5d94e81e9b4d79d
Sergio Bazan <drakeb2008@hotmail.com>
2023-08-19 10:35:37 -0500

    add class SergioBazan

---
+++ SergioBazan.java
@@ -0,0 +1,8 @@
+public class SergioBazan {
+    private Integer id;
+    private String lastName;
+    private String firstName;
+    public static void main(String[] args) {
+        System.out.println("Yo!");
+    }
+}
